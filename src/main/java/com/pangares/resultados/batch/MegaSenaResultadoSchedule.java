50077b9da5c65f1cdb123d851e490748c73c58cf
Alexandre Takashi <alexandretakashi@VF-1S.local>
2022-12-24 09:17:32 -0300

    feat: change cron time

--- src/main/java/com/pangares/resultados/batch/MegaSenaResultadoSchedule.java
+++ src/main/java/com/pangares/resultados/batch/MegaSenaResultadoSchedule.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MegaSenaResultadoSchedule {
 
-	@Scheduled(cron = "0 0 1 * * *")
+	@Scheduled(cron = "0 30 0 * * *")
 	public void schedulingForResultados() {
 		SpringApplication.run(BatchConfiguration.class, null);
 	}
